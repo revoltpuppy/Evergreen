efc3f5e3292601f083b61422332f93260f871758
Justin McDowell <justin@e081974.evergreen.edu>
2014-03-14 16:17:50 -0700

    Added lazy-fluid embedded content support.

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 var rem = 16;  //value of 1 root em
 var bWidth = document.documentElement.clientWidth; //browser width
-var illusRotationRange = 2;  //how far in either direction an illustration can rotate
+var illusRotationRange = 2;  //how far in either direction an illustration can rotate, in degrees
 
 /**
  * Slide components down from the top
@@ -113,35 +113,31 @@ $('.accordion > dt > a').click(function(event) {
  *
  * Push footer to the bottom when necessary.
  */
-var wrapper = $(".wrapper");
+/*var wrapper = $(".wrapper");
 var pageFooter = $(".page-footer");
 var dh = $("body").height() - $(document).height();
-//console.log(pageFooter.height());
-//console.log("document height: " + $(document).height() + "px");
-//console.log("body height: " + $("body").height() + "px");
-//console.log("dh == " + dh);
 function pushFooter(){
 	if(dh > 0){
     	wrapper.css("margin-bottom", (dh + pageFooter.height()) + "px");
     }
-}
+}*/
 
 /**
  * Fluid Multimedia
  * 
- * Fluid multimedia with default embed codes (makes things easy for content owners
+ * Fluid multimedia with default embed codes (makes things easy for content owners)
  * See: http://css-tricks.com/NetMag/FluidWidthVideo/demo.php
  */
-$(function(){
-	var $allVideos = $("iframe[src*='//player.vimeo'], iframe[src*='//www.youtube'], object, embed"),  //Get all videos
-	$fluidEl = $("figure");
+/*$(function(){
+	var $allVideos = $("iframe[src*='//player.vimeo'], iframe[src*='//www.youtube'], object, embed");  //Get all videos
+	//$fluidEl = $("figure");
     
 	$allVideos.each(function(){
 		//Get the width of the containing element
         $dataWidth = $(this).parent().width();
 		$(this)
 			//jQuery .data does not work on object/embed elements
-            .attr('data-width', $dataWidth)  //Set the target width
+			.attr('data-width', $dataWidth)  //Set the target width
 			.attr('data-aspectRatio', this.height / this.width)  //Set the video's aspect ratio
 			.removeAttr('height')  //Remove any explicitly specified height and width attributes
 			.removeAttr('width');
@@ -156,37 +152,96 @@ $(function(){
 				.height($el.attr('data-width') * $el.attr('data-aspectRatio'));
 		});
 	}).resize();
+});*/
+
+/**
+ * Fluid Media
+ * 
+ * Fluid multimedia with default embed codes (makes things easy for content owners)
+ * Adapted from Chris Coyier’s Fluid Width Video script
+ * See: http://css-tricks.com/NetMag/FluidWidthVideo/demo.php
+ */
+function fluidMedia(media){
+  media.each(function(){
+	  //Get the width of the containing element
+    $dataWidth = $(this).parent().width();
+    $(this)
+      //jQuery .data does not work on object/embed elements
+      .attr('data-width', $dataWidth)  //Set the target width
+      .attr('data-aspectRatio', this.height / this.width);  //Set the video's aspect ratio
+    fluidMediaResize(media);
+  });
+};
+function fluidMediaResize(media){
+  media.each(function(){
+    var $el = $(this);
+    $el  //Set the height and width of the element
+			.attr('data-width', $(this).parent().width())
+			.width($el.attr('data-width'))
+			.height($el.attr('data-width') * $el.attr('data-aspectRatio'));
+  });
+};
+
+
+/**
+ * Lazy Loading
+ * 
+ * Don’t load embedded YouTube content until user clicks on it.
+ */
+$(".lazy-load").click(function(e){
+	e.preventDefault();  //Disable the link
+	var src = $(this).attr("href").split("="),  //Get the unique video ID from the URL
+      width = $(this).width(),  //Get the dimensions of the current block for a seamless replacement
+      height = $(this).height(),
+      embededMedia = $("<iframe width='" + width + "' height='" + height + "' src='//www.youtube-nocookie.com/embed/" + src[1] + "?autohide=1&autoplay=1&rel=0' frameborder='0' allowfullscreen></iframe>");
+      //autohide hides the player controls, autoplay allows one-click video access, rel disables related content
+	$(this).replaceWith(embededMedia);  //Replace the link with the video
+  $allVideos = $("iframe[src*='//player.vimeo'], iframe[src*='//www.youtube'], object, embed");  //Get all videos again (with new video added in)
+  fluidMedia(embededMedia);  //Make it fluid for responsiveness!
 });
 
+/**
+ * Document is fully loaded
+ */
 $(document).ready(function(){
-	pushFooter();  //Push the footer when the page loads.
-    /*$(window).resize(pushFooter);*/  //Also if the browser window is resized.
-    $(window).resize(function(){
-    	//bWidth = document.documentElement.clientWidth; //browser width
-	    setTertiaryNav();
-	});
+	//pushFooter();  //Push the footer when the page loads.
+	/*$(window).resize(pushFooter);*/  //Also if the browser window is resized.
     
-    /**
-     * Random Rotation
-     * 
-     * Randomly rotate illustrations within a range.
-     */
-    $("img[class|='illus']").each(function(index){
-	    var degN = Math.random() * (illusRotationRange * 2) - illusRotationRange;
-	    //console.log(index + ": " + degN + " degrees");
-	    $(this).css("transform", "rotate(" + degN + "deg)");
-	});
-    $("img[class|='image']").each(function(index){
-	    var degN = Math.random() * (illusRotationRange * 2) - illusRotationRange;
-	    //console.log(index + ": " + degN + " degrees");
-	    $(this).css("transform", "rotate(" + degN + "deg)");
+	/**
+	 * Random Rotation
+	 * 
+	 * Randomly rotate illustrations within a range.
+	 */
+	$("img[class|='image'], img[class|='illus']").each(function(index){
+	  var degN = Math.random() * (illusRotationRange * 2) - illusRotationRange;
+	  //console.log(index + ": " + degN + " degrees");
+	  $(this).css("transform", "rotate(" + degN + "deg)");
 	});
 	
-	//var message = "<p>Lorem ipsum dolor sit amet.</p>";
+	/**
+	 * Alert
+	 *
+	 * Show timely and important information.
+	 */
 	var alertURL = "/_inc/js/alert.txt";
 	$.get(alertURL, function(message){
 		if(message != ""){
 		  $("#sitewide-alert").addClass("alert").append(message);
 		}
+	});
+	
+  /**
+   * Run the fluidMedia function on all embeded content
+   */
+  window.$allVideos = $("iframe[src*='//player.vimeo'], iframe[src*='//www.youtube'], object, embed");  //Get all videos
+  fluidMedia($allVideos);
+	
+	/**
+	 * Window Resizing
+	 */
+	$(window).resize(function(){
+		setTertiaryNav();
+		//Maintain aspect ratio during screen resizing
+		fluidMediaResize($allVideos);
 	});
 });
